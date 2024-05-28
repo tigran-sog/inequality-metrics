1e2a96b67330cec483cc958dac6bca10ff4bfdd1
Tigran S <tigransogomonian@gmail.com>
2024-05-28 18:49:31 +0100

    adding old generalised mean code

--- code/generalised mean.py
+++ code/generalised mean.py
@@ -37,26 +37,91 @@ def generalised_mean_dist(x, p):
         
 def generalised_mean3(dist,x,position,p):
     dist[position] = x
+    dist = np.array(dist, dtype=float)  # Ensure dist elements are floats
     if p == 0: # if p 0 then set it equal to the geometric mean
-        return gmean(x)
+        return gmean(dist)
     else:
         return (np.sum(np.power(dist,p)) / len(dist))**(1/p)
 
-E_array = [0.01,0.25,0.5,0.75,1,2,4,8,16]
-x_array = np.arange(0,4.01,0.01)
-dist = [1,2]
+E_array = [0,0.25,0.75,1,2,4,8]
 
+# Plotting generalised (1,x)
+x_array = np.arange(0,4.01,0.01)
+two_value_dist = [1,2]
 plt.figure(figsize=(10,10))
 for E in E_array:
     p = 1 - E
-    plt.plot(x_array, generalised_mean(x_array,p),
-             label = f'E = {E}')
+    plt.plot(x_array, np.array([generalised_mean3(two_value_dist, x, 1, p) for x in x_array]),
+             label = f'E = {E} (p = {p})')
+plt.title('Varying E for generalised means of simple mean of 1 and x')
+plt.xlabel('(1,x)')
+plt.ylabel('Generalisd mean')
 plt.tight_layout()
 plt.grid()
 plt.legend()
 plt.show()
 
 
+# Plotting generalised (2,x,6,8)
+four_value_dist = [2,4,6,8] # approximating income distribution 
+x_array = np.arange(2.01,6.00,0.01) # restricting x to be between first and third values
+plt.figure(figsize=(10,10))
+for E in E_array:
+    p = 1 - E
+    plt.plot(x_array, np.array([generalised_mean3(four_value_dist, x, 1, p) for x in x_array]),
+             label = f'E = {E} (p = {p})')
+plt.title('Varying E for generalised means of approximated income distributions')
+plt.ylabel('Generalisd mean')
+plt.xlabel('(2,x,6,8)')
+plt.tight_layout()
+plt.grid()
+plt.legend()
+plt.show()
+
+# Plotting generalised (4,x,12,16) [confirms that its only relative parts of the distribution that matters]
+four_value_dist = [4,8,12,16] # approximating income distribution 
+x_array = np.arange(4.01,12.00,0.01) # restricting x to be between first and third values
+plt.figure(figsize=(10,10))
+for E in E_array:
+    p = 1 - E
+    plt.plot(x_array, np.array([generalised_mean3(four_value_dist, x, 1, p) for x in x_array]),
+             label = f'E = {E} (p = {p})')
+plt.title('Varying E for generalised means of approximated income distributions')
+plt.ylabel('Generalisd mean')
+plt.xlabel('(4,x,12,16)')
+plt.tight_layout()
+plt.grid()
+plt.legend()
+plt.show()
+
+
+def plot_generalised_means(dist, position, x_range, E_array):
+   #x_array = np.arange(dist[position-1]+0.01,dist[position-1]+x_range,0.01)
+   x_array = np.arange(dist[position]-(x_range/2),dist[position]+(x_range/2),0.01)
+   
+   dist_title = dist.copy()
+   dist_title[position] = 'x'
+   plt.figure(figsize=(10,10))
+   for E in E_array:
+       p = 1 - E
+       plt.plot(x_array, np.array([generalised_mean3(dist, x, position, p) for x in x_array]),
+                label = f'E = {E} (p = {p})')
+   plt.title(f'Varying E for generalised means ({dist_title})')
+   plt.ylabel('Generalisd mean')
+   plt.xlabel(f'{dist_title}')
+   plt.tight_layout()
+   plt.grid()
+   plt.legend()
+   plt.show()
+
+plot_generalised_means(dist = [2,4,6,8], position = 0, x_range = 4, E_array = E_array)
+plot_generalised_means(dist = [2,4,6,8], position = 1, x_range = 4, E_array = E_array)
+plot_generalised_means(dist = [2,4,6,8], position = 2, x_range = 4, E_array = E_array)    
+plot_generalised_means(dist = [2,4,6,8], position = 3, x_range = 4, E_array = E_array)
+
+    
+##############################################################################
+##############################################################################
 income_dists = {
    'Nigeria' : [100,500,750,1500],
    'Armenia' : [100,250,500,650],
